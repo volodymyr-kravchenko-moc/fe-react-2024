4790556caaa9e20c13662dac5bb17059174add0d
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-05-01 21:21:22 +0300

    add the HeaderComponent and the FooterComponent to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { AboutComponent } from './components/about/About.component.tsx';
+import { FooterComponent } from './components/footer/Footer.component.tsx';
+import { HeaderComponent } from './components/header/Header.component.tsx';
 
 import styles from './App.module.css';
 
 function App() {
     return (
-        <main className={styles.mainContainer}>
-            <AboutComponent />
-        </main>
+        <>
+            <HeaderComponent />
+            <main className={styles.mainContainer}>
+                <AboutComponent />
+            </main>
+            <FooterComponent />
+        </>
     );
 }
 
