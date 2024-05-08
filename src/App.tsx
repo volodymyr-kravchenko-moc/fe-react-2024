45a49dee9a3f0ff368a4046b8812450edbcae028
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-05-08 21:42:56 +0300

    removed App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { AboutComponent } from './components/about/About.component.tsx';
 import { FooterComponent } from './components/footer/Footer.component.tsx';
 import { HeaderComponent } from './components/header/Header.component.tsx';
 
-import styles from './App.module.css';
-
 function App() {
     return (
         <>
             <HeaderComponent />
-            <main className={styles.mainContainer}>
+            <main>
                 <AboutComponent />
             </main>
             <FooterComponent />
