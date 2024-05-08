a73703620e4dc026bb49bbedb79a84ebf2a1677b
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-05-08 22:19:21 +0300

    removed SocialIconComponent and SocialIconProps interface

--- src/components/common/SocialIcon.component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import type { SocialIconProps } from '@/interfaces/SocialIconProps.ts';
-
-export const SocialIconComponent: React.FC<SocialIconProps> = ({ icon, url, customClass }) => (
-    <a className={customClass} href={url} target="_blank" rel="noopener noreferrer">
-        <img src={String(icon)} alt="Icon" />
-    </a>
-);
--- src/interfaces/SocialIconProps.ts
+++
@@ -1,7 +0,0 @@
-import type React from 'react';
-
-export interface SocialIconProps {
-    icon: React.ReactNode;
-    url: string;
-    customClass: string;
-}
