e5ed05237e5d325d6ccb3d07d9695fcb2309c9bc
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-05-09 21:17:34 +0300

    add type attr to button element in SocialIconButtonComponent

--- src/components/shared/SocialIconButtonComponent.tsx
+++ src/components/shared/SocialIconButtonComponent.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 import type { SocialIconButtonProps } from '@/interfaces/SocialIconButtonProps.ts';
 
 export const SocialIconButtonComponent: React.FC<SocialIconButtonProps> = ({ children, customClass }) => {
-    const buttonProps: any = customClass ? { className: customClass } : {};
+    const buttonProps: any = { type: 'button' };
+
+    if (customClass) {
+        buttonProps.className = customClass;
+    }
 
     return <button {...buttonProps}>{children}</button>;
 };
