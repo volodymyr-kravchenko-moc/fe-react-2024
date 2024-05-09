e95e615531ba7b7dd818b7868f4505cba455f4ad
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-05-09 21:28:10 +0300

    HeaderComponent refactoring

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,6 +1,6 @@
 import CartIcon from '@/assets/header/cart.svg?react';
-import LoginIcon from '@/assets/header/desktop/login.svg?react';
 import LogoIcon from '@/assets/header/desktop/logo.svg?react';
+import LogoutIcon from '@/assets/header/desktop/logout.svg?react';
 import MoonIcon from '@/assets/header/desktop/moon.svg?react';
 import SignUpIcon from '@/assets/header/desktop/sign-up.svg?react';
 import SunIcon from '@/assets/header/desktop/sun.svg?react';
@@ -12,38 +12,42 @@ import styles from './header.module.css';
 
 export const HeaderComponent = () => (
     <header className={styles.headerContainer}>
-        <div className={styles.firstMainWrapper}>
+        <div className={styles.logo}>
             <SocialIconLinkComponent url="/" target="_self">
                 <LogoIcon />
             </SocialIconLinkComponent>
-            <div className={styles.layout}>
-                <SocialIconButtonComponent customClass={styles.sun}>
-                    <SunIcon />
-                </SocialIconButtonComponent>
-                <SocialIconButtonComponent customClass={styles.moon}>
-                    <MoonIcon />
-                </SocialIconButtonComponent>
-            </div>
         </div>
-        <div className={styles.secondMainWrapper}>
-            <div className={styles.navBarWrapper}>
-                <button className={styles.navBarItem}>About</button>
-                <button className={styles.navBarItem}>Products</button>
-            </div>
-            <div className={styles.actionItemsWrapper}>
-                <SocialIconButtonComponent customClass={styles.cart}>
-                    <CartIcon />
-                </SocialIconButtonComponent>
-                <SocialIconButtonComponent customClass={styles.login}>
-                    <LoginIcon />
+        <div className={styles.appLayout}>
+            <SocialIconButtonComponent customClass={styles.sun}>
+                <SunIcon />
+            </SocialIconButtonComponent>
+            <SocialIconButtonComponent customClass={styles.moon}>
+                <MoonIcon />
+            </SocialIconButtonComponent>
+        </div>
+        <div className={styles.navBar}>
+            <button type="button" className={styles.activeNavBarItem}>
+                About
+            </button>
+            <button type="button" className={styles.navBarItem}>
+                Products
+            </button>
+        </div>
+        <div className={styles.actionItems}>
+            <SocialIconButtonComponent customClass={styles.cart}>
+                <CartIcon />
+            </SocialIconButtonComponent>
+            <div className={styles.loginWrapper}>
+                <SocialIconButtonComponent customClass={styles.logout}>
+                    <LogoutIcon />
                 </SocialIconButtonComponent>
                 <SocialIconButtonComponent customClass={styles.signUp}>
                     <SignUpIcon />
                 </SocialIconButtonComponent>
-                <SocialIconButtonComponent customClass={styles.menu}>
-                    <MenuIcon />
-                </SocialIconButtonComponent>
             </div>
+            <SocialIconButtonComponent customClass={styles.menu}>
+                <MenuIcon />
+            </SocialIconButtonComponent>
         </div>
     </header>
 );
