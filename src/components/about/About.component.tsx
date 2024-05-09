8aa3ffe70dd51b3b5256c588f3ab31a7a2ded7e1
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-05-09 21:21:45 +0300

    About component refactoring

--- src/components/about/About.component.tsx
+++ src/components/about/About.component.tsx
@@ -11,26 +11,30 @@ export const AboutComponent = () => {
         <section className={styles.wrapper}>
             <TitleComponent titleClasses={styles.aboutTitle}>About me</TitleComponent>
             <div className={styles.aboutDescription}>
-                <p>
+                <p className={styles.aboutText}>
                     Hi! My name is Volodymyr and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
                     React, HTML, CSS, JavaScript and Git version control system.
                 </p>
-                <p>
+                <p className={styles.aboutText}>
                     This page was developed during the course &apos;
-                    <a href="https://www.mastersacademy.education/frontend-for-beginners-it" target="_blank" rel="noreferrer">
-                        Intro to React
-                    </a>
+                    <span className={styles.aboutText}>
+                        <a href="https://www.mastersacademy.education/frontend-for-beginners-it" target="_blank" rel="noreferrer">
+                            Intro to React
+                        </a>
+                    </span>
                     &apos; from Masters Academy in 2024.
                 </p>
-                <p>
+                <p className={styles.aboutText}>
                     This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own
                     small project for the portfolio.
                 </p>
-                <p>
+                <p className={styles.aboutText}>
                     You can contact me via{' '}
-                    <a href="https://t.me/kvs2204" target="_blank" rel="noreferrer">
-                        Telegram
-                    </a>{' '}
+                    <span className={styles.aboutText}>
+                        <a href="https://t.me/kvs2204" target="_blank" rel="noreferrer">
+                            Telegram
+                        </a>{' '}
+                    </span>
                     and check out my{' '}
                     <a href="https://github.com/volodymyr-kravchenko-moc" target="_blank" rel="noreferrer">
                         GitHub
