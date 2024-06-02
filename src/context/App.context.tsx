208eaf7a86c4f2b12a19833ae9454656b6084bbb
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-06-02 21:06:56 +0300

    AppContextProvider and AppContext

---
+++ src/context/App.context.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { createContext, useEffect } from 'react';
+
+import type { AppContextProps } from '@/interfaces/AppContextProps.ts';
+import type { CartItemProps } from '@/interfaces/CartItemProps.ts';
+
+export const AppContext = createContext<AppContextProps>({
+    cartItems: [],
+    setCartItems: () => {},
+});
+
+export const AppContextProvider = ({ children }) => {
+    const [cartItems, setCartItems] = React.useState<CartItemProps[]>([]);
+    const [isInitialCartLoading, setInitialCartLoading] = React.useState<boolean>(true);
+
+    useEffect(() => {
+        const storedCartItems = localStorage.getItem('storedCartItems');
+        const extractedCartItems = storedCartItems ? JSON.parse(storedCartItems) : null;
+
+        extractedCartItems && setCartItems(extractedCartItems);
+        setInitialCartLoading(false);
+    }, []);
+
+    useEffect(() => {
+        if (isInitialCartLoading) {
+            return;
+        }
+
+        localStorage.setItem('storedCartItems', JSON.stringify(cartItems));
+    }, [cartItems]);
+
+    return <AppContext.Provider value={{ cartItems, setCartItems }}>{children}</AppContext.Provider>;
+};
