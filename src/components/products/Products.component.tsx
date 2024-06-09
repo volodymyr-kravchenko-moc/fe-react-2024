6ebc558632d09ba2df14d98f9c201ed5c1826507
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-06-09 17:43:28 +0300

    removed useEffect usage from ProductsComponent

--- src/components/products/Products.component.tsx
+++ src/components/products/Products.component.tsx
@@ -1,16 +1,4 @@
-import { useEffect, useState } from 'react';
-
 import productsFromApi from '@/assets/fake_data/products.json';
 import { ProductsListComponent } from '@/components/productList/ProductsList.component.tsx';
-import type { ProductProps } from '@/interfaces/ProductProps.ts';
 
-export const ProductsComponent = () => {
-    const page = 0;
-    const [products, setProducts] = useState<ProductProps[]>([]);
-
-    useEffect(() => {
-        setProducts(productsFromApi.slice(0, 20));
-    }, [page]);
-
-    return <ProductsListComponent products={products} />;
-};
+export const ProductsComponent = () => <ProductsListComponent products={productsFromApi.slice(0, 20)} />;
