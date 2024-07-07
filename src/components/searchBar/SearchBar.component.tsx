53481a87ea85f59ddccf48e58898265990ccbb28
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-07-07 21:28:28 +0300

    created SearchBar component

---
+++ src/components/searchBar/SearchBar.component.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+import SearchIcon from '@/assets/searchBar/search-icon.svg?react';
+import { SortingDropDownComponent } from '@/components/searchBar/sortingDropDown/SortingDropDown.component.tsx';
+
+import styles from './searchBar.module.css';
+
+export const SearchBar = () => {
+    const categories = ['Electronics', 'Shoes', 'Clothes'];
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    return (
+        <div className={styles.searchBar}>
+            <div className={styles.searchInputWrapper}>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className={styles.searchInput}
+                />
+                <SearchIcon className={styles.searchIcon} />
+            </div>
+            <div className={styles.selectionWrapper}>
+                <div className={styles.categorySelectionWrapper}>
+                    {categories.map((category) => {
+                        const buttonStyles =
+                            category === selectedCategory
+                                ? `${styles.activeCategoryButton} ${styles.categoryButton}`
+                                : styles.categoryButton;
+                        return (
+                            <button
+                                key={category}
+                                className={buttonStyles}
+                                onClick={() => {
+                                    setSelectedCategory(category);
+                                }}
+                            >
+                                {category}
+                            </button>
+                        );
+                    })}
+                </div>
+            </div>
+            <SortingDropDownComponent />
+        </div>
+    );
+};
