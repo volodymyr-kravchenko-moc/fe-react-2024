51fddd1157020b08b675f4e4c2b7233686e70637
Kravchenko <volodymyr.kravchenko@masterofcode.com>
2024-07-24 23:41:02 +0300

    close drop down by clicking on esc or outside of dropdown

--- src/components/searchBar/sortingDropDown/SortingDropDown.component.tsx
+++ src/components/searchBar/sortingDropDown/SortingDropDown.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { DropDownItemComponent } from '@/components/searchBar/DropDownItem/DropDownItem.component.tsx';
 import { SortingArrowClosedComponent } from '@/components/svg/SortingArrowClosed.component.tsx';
@@ -11,6 +11,7 @@ export const SortingDropDownComponent = () => {
     const [sortBy, setSortBy] = useState(SortingItem.PRICE_HIGH_LOW);
     const [isOpen, setIsOpen] = useState(false);
     const dropDownItems = Object.values(SortingItem);
+    const dropdownReference = useRef<HTMLDivElement | null>(null);
 
     const handleSortChange = (value: SortingItem) => {
         setSortBy(value);
@@ -21,8 +22,35 @@ export const SortingDropDownComponent = () => {
         setIsOpen(!isOpen);
     };
 
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+            setIsOpen(false);
+        }
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dropdownReference.current && !dropdownReference.current.contains(event.target as Node)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener('keydown', handleKeyDown);
+            document.addEventListener('mousedown', handleClickOutside);
+        } else {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [isOpen]);
+
     return (
-        <div className={styles.sortByWrapper}>
+        <div className={styles.sortByWrapper} ref={dropdownReference}>
             <span className={styles.sortByLabel}>Sort by:</span>
             {isOpen ? (
                 <div className={styles.dropdownMenuWrapper}>
